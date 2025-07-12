3562fcdbca3b0092dfadebfcb592a2f9b86b1a06
Saraswathidevi-S <160331322+Saraswathidevi-S@users.noreply.github.com>
2025-07-12 13:59:42 +0530

    Setup for GitHub Pages deploy

--- CrimeAna/vite.config.ts
+++ CrimeAna/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CrimeAna/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
